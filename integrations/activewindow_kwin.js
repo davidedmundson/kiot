2df35e6aaa7ac0ee1ec82f94bac56118b31635b8
David Edmundson <kde@davidedmundson.co.uk>
2025-11-27 09:53:26 +0000

    Spit the active window script into a separate file
    This is easier to work on and makes the integration side cleaner.

---
+++ integrations/activewindow_kwin.js
@@ -0,0 +1,52 @@
+var lastPayload = {};
+var currentWindow = null;
+
+function updateActiveWindow(w) {
+    if (!w) return;
+    if (w.transient && w.transientFor) w = w.transientFor;
+
+    var payload = {
+        title: w.caption || '',
+        resourceClass: w.resourceClass || '',
+        fullscreen: w.fullScreen.toString(),
+        screen: w.output.manufacturer,
+        x: w.x,
+        y: w.y,
+        width: w.width,
+        height: w.height,
+        pid: w.pid
+    };
+
+    var payloadStr = JSON.stringify(payload);
+    if (payloadStr !== JSON.stringify(lastPayload)) {
+        lastPayload = payload;
+        callDBus('org.davidedmundson.kiot.ActiveWindow', '/ActiveWindow', 'org.davidedmundson.kiot.ActiveWindow', 'UpdateAttributes', payloadStr);
+    }
+}
+
+function onCaptionChanged() {
+    updateActiveWindow(currentWindow);
+}
+
+function onGeometryChanged() {
+    updateActiveWindow(currentWindow);
+}
+
+function watchWindow(w) {
+    if (!w) return;
+
+    if (currentWindow) {
+        currentWindow.captionChanged.disconnect(onCaptionChanged);
+        currentWindow.frameGeometryChanged.disconnect(onGeometryChanged);
+    }
+
+    currentWindow = w;
+    currentWindow.captionChanged.connect(onCaptionChanged);
+    currentWindow.frameGeometryChanged.connect(onGeometryChanged);
+
+    updateActiveWindow(w);
+}
+
+watchWindow(workspace.activeWindow);
+workspace.windowActivated.connect(watchWindow);
+
