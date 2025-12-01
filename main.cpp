4d4aa1d2094b13175e778a7273b662342c8ee0f8
TheOddPirate <oddostlie@gmail.com>
2025-12-01 13:46:27 +0100

    Added a SIGTERM and SIGINT handler to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,29 @@
 #include <QApplication>
 #include <QDebug>
 
+#include "core.h"
 #include <KAboutData>
-#include <KSharedConfig>
 #include <KConfigGroup>
 #include <KDBusService>
-
-#include "core.h"
-
-int main(int argc, char ** argv)
+#include <KSharedConfig>
+#include <KSignalHandler>
+// Is there a way to get the signal directly from
+#include <csignal>
+int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
-    KAboutData aboutData(
-        QStringLiteral("kiot"),
-        "KDE IOT",
-        QStringLiteral("0.1"),
-        "KDE Internet of Things Connection",
-        KAboutLicense::GPL_V3,
-        "© 2024");
+    KAboutData aboutData(QStringLiteral("kiot"), "KDE IOT", QStringLiteral("0.1"), "KDE Internet of Things Connection", KAboutLicense::GPL_V3, "© 2024");
     KDBusService service(KDBusService::Unique);
     HaControl appControl;
+
+    KSignalHandler::self()->watchSignal(SIGTERM);
+    KSignalHandler::self()->watchSignal(SIGINT);
+    QObject::connect(KSignalHandler::self(), &KSignalHandler::signalReceived, [](int sig) {
+        if (sig == SIGTERM || sig == SIGINT) {
+            QApplication::quit();
+        }
+    });
+
     app.exec();
 }
 // SPDX-FileCopyrightText: 2025 David Edmundson <davidedmundson@kde.org>
